dd384de1c7c6f806dc95c126d88861c26c1259a0
Deepak <deepakkushwaha5945@gmail.com>
2026-03-23 11:27:39 +0530

    Footer Updated

--- src/common/Footer.jsx
+++ src/common/Footer.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 export default function Footer() {
   return (
     
-    <footer className="sticky shadow-lg   border-t-1 border-black  bottom-0 w-full bg-white text-black md:py-5  ">
+    <footer className="sticky shadow-lg  border-t-1 border-black  bottom-0 w-full bg-white text-black md:py-5  ">
          
             
-            <div className=" justify-center text-sm flex md:flex-row sm:gap-2 gap-0 flex-col sm:p-0 p-1">
-                <p>&copy; 2025 Admin Dashboard</p>
-                 <p >Developed by <span className="font-bold text-black">Deepak Kushwaha</span> </p>
+            <div className="flex sm:flex-row sm:gap-1 flex-col justify-center items-center gap-0">
+                <p className='py-0 text-[15px]'>&copy; 2025 Admin Dashboard</p>
+                 <p className='py-0 text-[15px]'>Developed by <span className="py-0 font-bold text-black">Deepak Kushwaha</span> </p>
             </div>
          
     </footer>
