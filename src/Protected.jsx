d5e81f2aa39271e4ac07302afa26e41648efef12
Deepak <deepakkushwaha5945@gmail.com>
2026-06-08 22:48:50 +0530

    ProtectionRoute code updated

--- src/Protected.jsx
+++ src/Protected.jsx
@@ -1,13 +1,56 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Navigate } from "react-router";
 
 
 export default function ProtectedRoute({ children }) {
 
-    const token = localStorage.getItem("token");
+    const [isValid, setIsValid] = useState(null);
 
-    if (!token) {
-        return <Navigate to="/" replace />;
+    useEffect(() => {
+
+        const verifyToken = async () => {
+
+            const token = localStorage.getItem("token");
+
+            if (!token) {
+                setIsValid(false);
+                return;
+            }
+
+            try {
+
+                const res = await axios.get(
+                    "https://my-portfolio-backend-2026.onrender.com/admin/verify-token",
+                    {
+                        headers: {
+                            Authorization: `Bearer ${token}`
+                        }
+                    }
+                );
+                if (res.data.status) {
+                    // console.log(res.data);
+                    setIsValid(res.data.status);
+                }
+
+
+            } catch (error) {
+
+                localStorage.removeItem("token");
+                setIsValid(false);
+
+            }
+        };
+
+        verifyToken();
+
+    }, []);
+
+    if (isValid === null) {
+        return <h1>Loading...</h1>;
     }
 
-    return children;
+    return isValid
+        ? children
+        : <Navigate to="/" replace />;
 }
--- src/main.jsx
+++ src/main.jsx
@@ -38,7 +38,7 @@ createRoot(document.getElementById('root')).render(
 
 
             {/* Admin Layout */}
-            <Route element={<ProtectedRoute> <Home /> </ProtectedRoute>}>
+            <Route element={<ProtectedRoute> <Home/> </ProtectedRoute>}>
 
 
                <Route path="dashboard" element={<Dashboard />} />
